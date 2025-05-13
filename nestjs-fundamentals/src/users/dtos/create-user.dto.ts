2f756a0283ba541792492a1935d5af96d59f8ecd
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-05-13 12:18:13 +0300

    docs: add comments for validation

--- nestjs-fundamentals/src/users/dtos/create-user.dto.ts
+++ nestjs-fundamentals/src/users/dtos/create-user.dto.ts
@@ -9,6 +9,9 @@ import {
 export class CreateUserDto {
   @IsString()
   @Length(3, 20)
+  // This will use pipes injected into routes not global pipes
+  // @Length(3, 20, { groups: ['create'] })
+  // @Length(6, 20, { groups: ['update'] })
   readonly username: string;
 
   @IsEmail()
