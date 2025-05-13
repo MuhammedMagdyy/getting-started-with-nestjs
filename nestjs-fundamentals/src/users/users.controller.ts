63021db9ec360c52faaf2e5507565dd9d619f54e
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-05-13 17:26:07 +0300

    feat: implement user service for seperation of concerns

--- nestjs-fundamentals/src/users/users.controller.ts
+++ nestjs-fundamentals/src/users/users.controller.ts
@@ -5,58 +5,41 @@ import {
   Get,
   HttpCode,
   HttpStatus,
-  NotFoundException,
   Param,
   Patch,
   Post,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { UserEntity } from './user.entity';
+import { UserService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  private users: UserEntity[] = [];
+  constructor(private readonly userService: UserService) {}
 
   @Get('/')
-  findAll(): UserEntity[] {
-    return this.users;
+  findAll() {
+    return this.userService.findAll();
   }
 
   @Get('/:id')
-  findOne(@Param('id') id: string): UserEntity {
-    const user = this.users.find((user) => user.id === id);
-    if (!user) {
-      throw new NotFoundException(`User with id ${id} not found`);
-    }
-    return user;
+  findOne(@Param('id') id: string) {
+    return this.userService.findUserById(id);
   }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    const user: UserEntity = {
-      id: Math.random().toString(10).substring(2),
-      ...createUserDto,
-    };
-    this.users.push(user);
-    return user;
+    return this.userService.createUser(createUserDto);
   }
 
   @Patch('/:id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    const idx = this.users.findIndex((user) => user.id === id);
-    if (idx === -1) {
-      throw new NotFoundException(`User with id ${id} not found`);
-    }
-    // This overrides the current entity
-    this.users[idx] = { ...this.users[idx], ...updateUserDto };
-
-    return this.users[idx];
+    return this.userService.updateUser(id, updateUserDto);
   }
 
   @Delete('/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
-    this.users = this.users.filter((user) => user.id !== id);
+    return this.userService.deleteUser(id);
   }
 }
---
+++ nestjs-fundamentals/src/users/users.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserEntity } from './user.entity';
+
+@Injectable()
+export class UserService {
+  private users: UserEntity[] = [];
+
+  findAll(): UserEntity[] {
+    return this.users;
+  }
+
+  findUserById(id: string): UserEntity {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    return user;
+  }
+
+  createUser(createUserDto: CreateUserDto): UserEntity {
+    const user: UserEntity = {
+      id: Math.random().toString(10).substring(2),
+      ...createUserDto,
+    };
+    this.users.push(user);
+    return user;
+  }
+
+  updateUser(id: string, updateUserDto: UpdateUserDto): UserEntity {
+    const idx = this.users.findIndex((user) => user.id === id);
+    if (idx === -1) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    // This overrides the current entity
+    this.users[idx] = { ...this.users[idx], ...updateUserDto };
+
+    return this.users[idx];
+  }
+
+  deleteUser(id: string): void {
+    this.users = this.users.filter((user) => user.id !== id);
+  }
+}
