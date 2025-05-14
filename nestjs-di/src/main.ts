97f21c6d884fc6089e08ddc05ef78676b1c09d74
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-05-14 13:44:59 +0300

    feat: initialize NestJS DI application

---
+++ nestjs-di/src/main.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
+import { AppModule } from './app.module';
+
+const port = process.env.PORT ?? 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+}
+
+bootstrap()
+  .then(() => {
+    console.log(`Server is running on port ${port}`);
+  })
+  .catch((error) => {
+    console.error('Error starting the server:', error);
+    process.exit(1);
+  });
