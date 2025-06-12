ee1f561ae0344c5482a5a72b389860d50bfd4597
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-06-13 00:31:11 +0300

    feat: implement user registration with validation and password hashing

--- simple-ecommerce-api/src/users/users.controller.ts
+++ simple-ecommerce-api/src/users/users.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterDto } from './dtos/register.dto';
 import { UsersService } from './users.service';
 
 @Controller('/api/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
+  @Post('/auth/register')
+  async register(@Body() body: RegisterDto) {
+    return this.usersService.register(body);
   }
 }
--- simple-ecommerce-api/src/users/users.service.ts
+++ simple-ecommerce-api/src/users/users.service.ts
@@ -1,26 +1,39 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
-import { ReviewsService } from 'src/reviews/reviews.service';
-
-type User = {
-  id: number;
-  name: string;
-  email: string;
-};
+import { ConflictException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { genSalt, hash } from 'bcryptjs';
+import { Repository } from 'typeorm';
+import { RegisterDto } from './dtos/register.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
-    @Inject(forwardRef(() => ReviewsService))
-    private readonly reviewsService: ReviewsService,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly configService: ConfigService,
   ) {}
 
-  private users: User[] = [
-    { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
-    { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' },
-    { id: 3, name: 'Alice Johnson', email: 'alice.johnson@example.com' },
-  ];
+  async register(registerDto: RegisterDto) {
+    const { email, password, username } = registerDto;
+    const userExists = await this.userRepository.findOne({
+      where: { email },
+    });
 
-  findAll(): User[] {
-    return this.users;
+    if (userExists) {
+      throw new ConflictException(`User with email ${email} already exists.`);
+    }
+
+    const hashSaltRounds = this.configService.get<string>('HASH_SALT_ROUNDS');
+    const salt = await genSalt(Number(hashSaltRounds));
+    const hashedPassword = await hash(password, salt);
+
+    const newUser = this.userRepository.create({
+      email,
+      password: hashedPassword,
+      username,
+    });
+
+    // TODO: Generate JWT token
+    return await this.userRepository.save(newUser);
   }
 }
