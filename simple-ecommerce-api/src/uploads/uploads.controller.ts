f6ef979e60c4b96a8f5e8bcc1367070479ba95b8
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-06-14 11:17:16 +0300

    feat: add support for uploading multiple files

--- simple-ecommerce-api/src/uploads/uploads.controller.ts
+++ simple-ecommerce-api/src/uploads/uploads.controller.ts
@@ -6,9 +6,10 @@ import {
   Post,
   Res,
   UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { Express, Response } from 'express';
 
 @Controller('/api/uploads')
@@ -23,6 +24,18 @@ export class UploadsController {
     }
 
     return { message: 'File uploaded successfully' };
+  }
+
+  @Post('/multiple-files')
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadMultipleFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
+    if (!files || files.length === 0) {
+      throw new BadRequestException(
+        'Files are not provided or are invalid. Please upload valid files.',
+      );
+    }
+
+    return { message: 'Files uploaded successfully' };
   }
 
   @Get('/:image')
