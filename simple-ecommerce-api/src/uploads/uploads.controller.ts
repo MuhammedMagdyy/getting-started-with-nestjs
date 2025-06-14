1e4898c9dbc7bac2558f409774b4b6856391fbda
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-06-14 11:11:07 +0300

    refactor: change file upload options to module

--- simple-ecommerce-api/src/uploads/uploads.controller.ts
+++ simple-ecommerce-api/src/uploads/uploads.controller.ts
@@ -10,39 +10,11 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express, Response } from 'express';
-import { diskStorage } from 'multer';
-import { MAX_FILE_SIZE } from 'src/common/utils/constants';
 
 @Controller('/api/uploads')
 export class UploadsController {
   @Post()
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './images',
-        filename: (_req, file, cb) => {
-          const prefix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-          const fileName = `${prefix}-${file.originalname}`;
-          cb(null, fileName);
-        },
-      }),
-      fileFilter: (_req, file, cb) => {
-        const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-        if (!allowedTypes.includes(file.mimetype)) {
-          return cb(
-            new BadRequestException(
-              'Invalid file type. Only JPEG, PNG, and JPG files are allowed.',
-            ),
-            false,
-          );
-        }
-        cb(null, true);
-      },
-      limits: {
-        fileSize: MAX_FILE_SIZE,
-      },
-    }),
-  )
+  @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
       throw new BadRequestException(
--- simple-ecommerce-api/src/uploads/uploads.module.ts
+++ simple-ecommerce-api/src/uploads/uploads.module.ts
@@ -1,9 +1,37 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { MAX_FILE_SIZE } from 'src/common/utils/constants';
 import { UploadsController } from './uploads.controller';
 
 @Module({
   controllers: [UploadsController],
-  imports: [MulterModule.register()],
+  imports: [
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './images',
+        filename: (_req, file, cb) => {
+          const prefix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const fileName = `${prefix}-${file.originalname}`;
+          cb(null, fileName);
+        },
+      }),
+      fileFilter: (_req, file, cb) => {
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+        if (!allowedTypes.includes(file.mimetype)) {
+          return cb(
+            new BadRequestException(
+              'Invalid file type. Only JPEG, PNG, and JPG files are allowed.',
+            ),
+            false,
+          );
+        }
+        cb(null, true);
+      },
+      limits: {
+        fileSize: MAX_FILE_SIZE,
+      },
+    }),
+  ],
 })
 export class UploadsModule {}
